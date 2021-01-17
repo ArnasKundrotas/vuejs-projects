3e273775bd59ae83936b13e6078b9cc30e08c732
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-01-17 22:38:39 +0200

    commit

--- Events1/app.js
+++ Events1/app.js
@@ -2,7 +2,42 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastname: ''
+      // lastname: ''
+    }
+  },
+  watch: {
+    counter(val){
+      if(val > 15) {
+        const that = this;
+        setTimeout(() => {
+          this.counter = 0;
+        },2000)
+      }
+    }
+    // name(val) {
+    //   if(val === ''){
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = val + ' ' + this.lastname;
+    //   }
+    // },
+    // lastname(val) {
+    //   if(val === ''){
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + val;
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      if(this.name === '' || this.lastname === '') {
+        return '';
+      } else {
+        return this.name + ' ' + this.lastname;
+      }
     }
   },
   methods: {
@@ -23,6 +58,14 @@ const app = Vue.createApp({
 
     reset() {
       this.name = '';
+    },
+
+    outFullName() {
+      if(this.name === '') {
+        return '';
+      } else {
+        return this.name + ' ' + 'Kundrotas';
+      }
     }
 
   }
