2e0523b4d5cb2e762b8e3f04f5ed1fa1e77ff617
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-01-19 21:51:55 +0200

    commit

--- Props/src/App.vue
+++ Props/src/App.vue
@@ -3,9 +3,24 @@
     <header>
       <h1>My Friends</h1>
     </header>
+
+    <new-friend
+      @add-contact="addContact"
+    ></new-friend>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+
+      <friend-contact 
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
+
     </ul>
   </section>
 </template>
@@ -16,24 +31,48 @@ export default {
     return {
       friends: [
         {
-          id: "manuel",
-          name: "Manuel Lorenz",
+          id: "Arnas",
+          name: "Arnas K Loreno",
           phone: "0123 45678 90",
-          email: "manuel@localhost.com",
+          email: "arnas@localhost.com",
+          isFavorite: true
         },
         {
-          id: "julie",
-          name: "Julie Jones",
+          id: "Arnas 2",
+          name: "Arnas Jones Alex",
           phone: "0987 654421 21",
-          email: "julie@localhost.com",
+          email: "alex@localhost.com",
+          isFavorite: false
         },
       ],
     };
   },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find((friend) => friend.id === friendId);
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+
+    addContact(name, phone, email) {
+      const newfriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      };
+      this.friends.push(newfriendContact);
+    },
+
+    deleteContact(friendId) {
+      this.friends = this.friends.filter(friend => friend.id !== friendId);
+    }
+  }
 };
 </script>
 
 <style>
+
 * {
   box-sizing: border-box;
 }
@@ -59,7 +98,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -89,4 +129,21 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
+}
+
 </style>
