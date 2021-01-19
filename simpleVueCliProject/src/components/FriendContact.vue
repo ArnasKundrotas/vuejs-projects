01f8e5520bcd9e8303d07e172eadc594715907dc
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-01-19 16:02:40 +0200

    commit

---
+++ simpleVueCliProject/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <!-- <button @click="toggleDetials">Show detials</button> -->
+        <button @click="toggleDetials">{{ detialAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detialAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detialAreVisible: false,
+            friend: {
+                    id: 'arnas',
+                    name: 'Arnas K',
+                    phone: '000 000 0001',
+                    email: 'arnas@gmail.com'
+                }
+        };
+    },
+    methods: {
+        toggleDetials() {
+            this.detialAreVisible = !this.detialAreVisible;
+        }
+    }
+}
+</script>
